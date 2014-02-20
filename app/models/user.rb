75a95f6360daed6bfabc270e7337a3bda825c8dd
Zack Olson <zsolson@gmail.com>
2014-02-19 23:10:27 -0600

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { email.downcase! }
   before_create :create_remember_token
 
@@ -9,6 +10,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def feed
+  	# This is preliminary. See "Following users" for the full implementation.
+  	Micropost.where("user_id = ?", id)
+  end
 
   def User.new_remember_token
   	SecureRandom.urlsafe_base64
